f10c8e8022c158b295ad0ed42a71dd55d27ac219
mobaswirul <mobaswirul@cse.uiu.ac.bd>
2024-11-13 10:34:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-var mysql = require('mysql2');
-const express = require('express');
+    // Importing the required packages
+    const mysql = require('mysql2');
+    const cors = require('cors');
+    const express = require('express');
+    const app = express();
 
-const app = express();
-const port = 3000;
+    // Setting up the express server
+    app.use(cors());
+    const port = 3000;
+    app.use(express.json());
 
-const cors = require('cors');
-app.use(cors());
+    // Connecting to the MySQL database
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "admin",
+        database: "hr"
+    });
 
-app.use(express.json());
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "hr"
-});
-
-con.connect(function(err) {
-    if(err) throw err;
-    console.log("Connected!"); 
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/employees', (req, res) => {
-    con.query("SELECT * FROM employees", function(err, result, fields) {
+    con.connect(function(err) {
         if(err) throw err;
-        res.send(result);
-    }
-    );
-});
+        console.log("Connected!"); 
+    });
 
-app.listen(port, () => {
-    console.log(`Express server running: http://localhost:${port}`);
-});
+    // Creating a simple hello world route
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+
+    // Creating a route to get all employees from the database
+    app.get('/employees', (req, res) => {
+        con.query("SELECT * FROM employees", function(err, result, fields) {
+            if(err) throw err;
+            res.send(result);
+        }
+        );
+    });
+
+    // Starting the server
+    app.listen(port, () => {
+        console.log(`Express server running: http://localhost:${port}`);
+    });
